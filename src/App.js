e24d395dbe28c819e86700ed7e2b606fbc938181
Sebastian Ammon <seb.ammon@gmail.com>
2020-03-29 13:13:22 +0700

    Handle value changes and save in localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
 import { Typography } from 'antd';
@@ -9,7 +9,19 @@ import { ID } from './utils';
 const { Title } = Typography;
 
 const App = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(() => {
+        try {
+            const item = window.localStorage.getItem('data');
+            return item ? JSON.parse(item) : [];
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem('data', JSON.stringify(data));
+    }, [data]);
 
     const handleAdd = () => {
         const len = data.length;
@@ -31,11 +43,34 @@ const App = () => {
         setData(newData);
     };
 
+    const handleScoreChange = (key, val) => {
+        const newData = [...data];
+        const idx = newData.findIndex((record) => record.key.toString() === key.toString());
+        const item = newData[idx];
+        newData.splice(idx, 1, { ...item, points: item.points + val });
+
+        setData(newData);
+    };
+
+    const handleParticipatationChange = (key, val) => {
+        const newData = [...data];
+        const idx = newData.findIndex((record) => record.key.toString() === key.toString());
+        const item = newData[idx];
+        newData.splice(idx, 1, { ...item, participation: val });
+
+        setData(newData);
+    };
+
     return (
         <div className={'container'}>
             <Title>Scoring App</Title>
             <ControlBar handleAdd={handleAdd} handleReset={handleReset} />
-            <ScoreTable data={data} handleDelete={handleDelete} />
+            <ScoreTable
+                data={data}
+                handleDelete={handleDelete}
+                handleScoreChange={handleScoreChange}
+                handleParticipatationChange={handleParticipatationChange}
+            />
         </div>
     );
 };
--- src/components/ScoreTable/index.js
+++ src/components/ScoreTable/index.js
@@ -17,6 +17,14 @@ const columns = (props) => {
         props.handleDelete && props.handleDelete(key);
     };
 
+    const handleScoreClick = (key, val) => () => {
+        props.handleScoreChange && props.handleScoreChange(key, val);
+    };
+
+    const onParticipatationChange = (key) => (val) => {
+        props.handleParticipatationChange && props.handleParticipatationChange(key, val);
+    };
+
     return [
         {
             title: 'Name',
@@ -45,12 +53,22 @@ const columns = (props) => {
             key: 'points',
             dataIndex: 'points',
             width: '30%',
-            render: (score) => {
+            render: (score, record) => {
                 return (
                     <div>
                         <span className={'score'}>{score}</span>
-                        <Button shape={'circle'} type={'primary'} icon={<PlusOutlined />} />
-                        <Button type={'primary'} shape={'circle'} icon={<MinusOutlined />} />
+                        <Button
+                            shape={'circle'}
+                            type={'primary'}
+                            icon={<PlusOutlined />}
+                            onClick={handleScoreClick(record.key, 1)}
+                        />
+                        <Button
+                            type={'primary'}
+                            shape={'circle'}
+                            icon={<MinusOutlined />}
+                            onClick={handleScoreClick(record.key, -1)}
+                        />
                     </div>
                 );
             },
@@ -59,9 +77,18 @@ const columns = (props) => {
             title: 'Participation',
             key: 'participation',
             dataIndex: 'participation',
-            render: (score) => (
+            render: (score, record) => (
                 <div style={{ marginRight: '1rem' }}>
-                    <Slider tipFormatter={null} marks={marks} step={null} defaultValue={score || 0} min={0} max={5} />
+                    <Slider
+                        tipFormatter={null}
+                        marks={marks}
+                        step={null}
+                        defaultValue={0}
+                        min={0}
+                        max={5}
+                        value={score}
+                        onChange={onParticipatationChange(record.key)}
+                    />
                 </div>
             ),
         },
