2a311ddfa9cecc85d573648a4b0c6c7f8c951705
Sebastian Ammon <seb.ammon@gmail.com>
2020-08-02 23:08:54 +0700

    Refactor component naming

---
+++ src/components/Name/Name.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Input, Typography, Popconfirm } from 'antd';
+import { DeleteOutlined, EditOutlined, CheckOutlined, CloseOutlined } from '@ant-design/icons';
+import './Name.css';
+
+const { Title } = Typography;
+
+const Name = (props) => {
+    const { record, name, handleDeleteClick, editingKey } = props;
+    const [value, setValue] = useState();
+
+    useEffect(() => {
+        if (!record.isNew) {
+            setValue(name);
+        }
+    }, [name, record]);
+
+    const isEditing = record.key === editingKey;
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleSave = () => {
+        props.handleSave && props.handleSave(record.key, record.isNew && !value ? name : value);
+    };
+
+    const handleEdit = () => {
+        props.handleEdit && props.handleEdit(record.key);
+    };
+
+    const handleEscape = (e) => {
+        if (e.keyCode === 27) {
+            props.handleSave(record.key, name);
+        }
+    };
+
+    const handleCancel = () => {
+        props.handleSave(record.key, name);
+    };
+
+    return (
+        <div className={'name-cell'}>
+            {isEditing ? (
+                <Input
+                    autoFocus={true}
+                    className={'name-input'}
+                    defaultValue={record.isNew ? undefined : name}
+                    placeholder={record.isNew ? 'Name' : undefined}
+                    allowClear={true}
+                    value={value}
+                    onChange={handleChange}
+                    onPressEnter={handleSave}
+                    onKeyDown={handleEscape}
+                />
+            ) : (
+                <Title level={2} className={'name'}>
+                    {name}
+                </Title>
+            )}
+            <div className={'button-container'}>
+                {isEditing ? (
+                    <React.Fragment>
+                        <Button onClick={handleSave} icon={<CheckOutlined />} type={'link'} />
+                        <Button onClick={handleCancel} icon={<CloseOutlined />} type={'link'} />
+                    </React.Fragment>
+                ) : (
+                    <Button onClick={handleEdit} icon={<EditOutlined />} type={'link'} />
+                )}
+                <Popconfirm
+                    title={'Delete this name?'}
+                    okText="Yes"
+                    cancelText="No"
+                    onConfirm={handleDeleteClick(record.key)}
+                >
+                    <Button
+                        style={{ color: 'red' }}
+                        icon={<DeleteOutlined />}
+                        type={'link'}
+                        className={'action-button'}
+                    />
+                </Popconfirm>
+            </div>
+        </div>
+    );
+};
+
+export default Name;
---
+++ src/components/ScoreTable/ScoreTable.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { Button, Slider, Table, Typography } from 'antd';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import './ScoreTable.css';
+import Name from '../Name';
+import face01 from '../../assets/face-icons/face-01.svg';
+import face02 from '../../assets/face-icons/face-02.svg';
+import face03 from '../../assets/face-icons/face-03.svg';
+import face04 from '../../assets/face-icons/face-04.svg';
+import face05 from '../../assets/face-icons/face-05.svg';
+
+const { Title } = Typography;
+
+const marks = {
+    0: <img className={'face'} src={face01} alt={'Face 01'} />,
+    1: <img className={'face'} src={face02} alt={'Face 02'} />,
+    2: <img className={'face'} src={face03} alt={'Face 03'} />,
+    3: <img className={'face'} src={face04} alt={'Face 04'} />,
+    4: <img className={'face'} src={face05} alt={'Face 05'} />,
+};
+
+const columns = (props) => {
+    const { data = [] } = props;
+    const max = data.reduce((max, curr) => (curr.points > max ? curr.points : max), 1);
+
+    const handleDeleteClick = (key) => () => {
+        props.handleDelete && props.handleDelete(key);
+    };
+
+    const handleScoreClick = (key, val) => () => {
+        props.handleScoreChange && props.handleScoreChange(key, val);
+    };
+
+    const onParticipationChange = (key) => (val) => {
+        props.handleParticipatationChange && props.handleParticipatationChange(key, val);
+    };
+
+    return [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            width: '35%',
+            render: (name, record) => (
+                <Name {...props} name={name} record={record} handleDeleteClick={handleDeleteClick} />
+            ),
+        },
+        {
+            title: 'Points',
+            key: 'points',
+            dataIndex: 'points',
+            width: '30%',
+            render: (score, record) => (
+                <div className={'score-cell'}>
+                    <div className={'score-container'}>
+                        <Title
+                            className={'score'}
+                            style={{ color: score > 0 ? 'green' : score < 0 ? '#b90000' : 'grey' }}
+                            level={2}
+                        >
+                            {score}
+                        </Title>
+                        <div className={`crown-container ${score === max ? 'show jello-horizontal' : 'hide'}`}>
+                            <i className={'fas fa-crown crown'} />
+                        </div>
+                    </div>
+                    <div>
+                        <Button
+                            style={{ marginRight: '2px' }}
+                            shape={'circle'}
+                            type={'primary'}
+                            icon={<PlusOutlined />}
+                            onClick={handleScoreClick(record.key, 1)}
+                        />
+                        <Button
+                            type={'primary'}
+                            shape={'circle'}
+                            icon={<MinusOutlined />}
+                            onClick={handleScoreClick(record.key, -1)}
+                        />
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: 'Effort',
+            key: 'effort',
+            dataIndex: 'effort',
+            render: (score, record) => (
+                <Slider
+                    className={'effort-slider'}
+                    tipFormatter={null}
+                    marks={marks}
+                    step={null}
+                    defaultValue={0}
+                    min={0}
+                    max={4}
+                    value={score}
+                    onChange={onParticipationChange(record.key)}
+                />
+            ),
+        },
+    ];
+};
+
+const ScoreTable = (props) => {
+    const { data = [] } = props;
+
+    return (
+        <div className={'table-container'}>
+            <Table
+                scroll={{ y: 'calc(100vh - 210px)' }}
+                pagination={false}
+                columns={columns(props)}
+                dataSource={data}
+            />
+        </div>
+    );
+};
+
+export default ScoreTable;
