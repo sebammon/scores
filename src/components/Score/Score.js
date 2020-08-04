a4774b2eba6619d40b1a2737bd45bc93a89dd986
Sebastian Ammon <seb.ammon@gmail.com>
2020-08-04 11:38:23 +0700

    Fix eslint issues

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Button, Typography } from 'antd';
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 
