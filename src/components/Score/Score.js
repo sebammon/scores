0f1a5eb1857e482894d871ac466f114f82e81df9
Sebastian Ammon <seb.ammon@gmail.com>
2020-08-06 17:17:18 +0700

    Move and update styles for points

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Button, Typography } from 'antd';
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import './Score.css';
 
 const { Title } = Typography;
 
 function Score(props) {
     const { handleScoreClick, record, score, max } = props;
 
+    const crownClassName = score === max ? 'show jello-horizontal' : 'hide';
+
     return (
         <div className={'score-cell'}>
-            <div className={'score-container'}>
-                <Title
-                    className={'score'}
-                    style={{ color: score > 0 ? 'green' : score < 0 ? '#b90000' : 'grey' }}
-                    level={2}
-                >
-                    {score}
-                </Title>
-                <div className={`crown-container ${score === max ? 'show jello-horizontal' : 'hide'}`}>
-                    <i className={'fas fa-crown crown'} />
-                </div>
+            <div className={`crown-container ${crownClassName}`}>
+                <i className={'fas fa-crown crown'} />
             </div>
+            <Title
+                className={'score'}
+                style={{ color: score > 0 ? '#237804' : score < 0 ? '#b90000' : 'grey' }}
+                level={2}
+            >
+                {score}
+            </Title>
             <div>
                 <Button
                     style={{ marginRight: '2px' }}
